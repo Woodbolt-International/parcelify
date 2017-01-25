aee27fdf1cdc7da0139f9ceb98cb4f72ae4034ed
Daniel Dixon <code@danieldixon.com>
2017-01-25 10:30:04 -0800

    Adding 'code' to allowed parameters.

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -59,6 +59,7 @@ class RatesController < ShopifyApp::AuthenticatedController
       :max_price,
       :min_grams,
       :max_grams,
+      :code,
       conditions_attributes: condition_params,
       product_specific_prices_attributes: product_specific_price_params
     )
